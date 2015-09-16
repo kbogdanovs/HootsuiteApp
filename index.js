94bfbca0e60c6c2c40a1d9727f8b4e611b9f9f33
kbogdanovs <kalebogdanovs@Kales-MacBook-Air.local>
2015-09-16 16:50:05 -0400

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var fs = require('fs')
-var request = require('request')
+var fs = require('fs');
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,31 +12,30 @@ app.use("/asset", express.static(__dirname + '/asset'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('pages/demo');
 });
 
-app.post('/', function(req, res) {
-	res.render('pages/demo')
+app.post('/', function (req, res) {
+    res.render('pages/demo')
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+var formData = {
+        apiKey: '6b300dd6-1896-4663-a5e8-121aabb9ae21',
+        projectId: '51df0cf05',
+        file: [fs.createReadStream(__dirname + '/test.properties')],
+        fileURI: 'test.properties'
+    };
 app.get('/test/', function(req, res) {
-	var formData = {
-		apiKey: '6b300dd6-1896-4663-a5e8-121aabb9ae21',
-		projectId: '51df0cf05',
-		file: [fs.createReadStream(__dirname + '/test.properties')],
-		fileURI: 'test.properties'
-	}
-	request.post({url:'https://api.smartling.com/v1/file/upload', formData: formData}, function optionalCallback(err, httpResponse, body) {
+    request.post({url:'https://api.smartling.com/v1/file/upload', formData: formData}, function optionalCallback(err, httpResponse, body) {
   if (err) {
     return console.error('upload failed:', err);
   }
   console.log('Upload successful!  Server responded with:', body);
-});
-	res.render('pages/demo')
 })
+   res.render('pages/demo');
+});
