1b635306aba6541545e619bf19d9fc8296f94a94
kbogdanovs <kalebogdanovs@Kales-MacBook-Air.local>
2015-09-17 18:00:28 -0400

    tweettest

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ var fs = require('fs');
 var request = require('request');
 var mustache = require('mustache');
 var Uploader = require('./upload')
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use("/asset", express.static(__dirname + '/asset'));
+app.use(express.bodyParser());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -26,6 +40,13 @@ app.get('/upload', function (req, res) {
     var uploader = new Uploader;
     uploader.upload('./test2.json');
     res.render('pages/uploadresult');
+});
+
+app.post('/tweet', function (req, res) {
+	var body = req.body.tweetContent;
+	var jsonText = '{"text" : "' + body + '"}';
+	fs.writeFile('tweet1.json', jsonText);
+	uploader.upload('tweet1.json');
 })
 
 app.listen(app.get('port'), function() {
