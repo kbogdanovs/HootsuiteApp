5a3d2c5deda1841c3b8c088042494e6508fa58c5
smartlingtestdocs <KaleBogdanovs@Kales-MacBook-Pro.local>
2015-10-29 21:26:43 -0400

    more

--- upload.js
+++ upload.js
@@ -6,6 +6,7 @@ var Uploader = function() {
 
 var mustache = require('mustache');
 var Contextualizer = require('./contextualize');
+var request = require('request')
 
 Uploader.prototype.upload = function(file, text, avatar, image) {
 
@@ -20,7 +21,7 @@ for (x=0; x < locales.length; x++) {
 
 var options = { method: 'POST',
   url: 'https://api.smartling.com/v1/file/upload',
-  qs: 
+  qs:
    { file: '@' + file,
      apiKey: 'd39e8d85-3d7b-46f5-ad79-30edf0ccf7b3',
      projectId: 'c5c7a69ed',
@@ -37,6 +38,6 @@ request(options, function (error, response, body) {
   console.log(body);
   var contextualizer = new Contextualizer;
   contextualizer.contextualize(text, avatar, image);
-};
+});
 
 module.exports = Uploader;
