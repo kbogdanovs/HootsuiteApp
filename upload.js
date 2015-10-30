cf1a8980c738887d144e29f4034bc5a94728dc86
smartlingtestdocs <KaleBogdanovs@Kales-MacBook-Pro.local>
2015-10-29 21:11:14 -0400

    request module test

--- upload.js
+++ upload.js
@@ -3,6 +3,7 @@
 var Uploader = function() {
 
 };
+
 var mustache = require('mustache');
 var Contextualizer = require('./contextualize');
 
@@ -17,30 +18,24 @@ for (x=0; x < locales.length; x++) {
 	authLocales += ' -F "localesToApprove=' + locales[x] + '"';
 }
 
-var curlOptions = {
-	filepath: file,
-	localesToApprove: authLocales,
-	callbackUrl: 'https://hsnodesmartlingsocial.herokuapp.com/translated/',
-	apiKey: 'd39e8d85-3d7b-46f5-ad79-30edf0ccf7b3',
-	projectId: 'c5c7a69ed'
-}
+var options = { method: 'POST',
+  url: 'https://api.smartling.com/v1/file/upload',
+  qs: 
+   { file: '@' + file,
+     apiKey: 'd39e8d85-3d7b-46f5-ad79-30edf0ccf7b3',
+     projectId: 'c5c7a69ed',
+     fileType: 'json',
+     fileUri: file,
+     localesToApprove: authLocales,
+     callbackUrl: 'https://hsnodesmartlingsocial.herokuapp.com/translated/',
+ };
 
-var command = mustache.render('curl -F "file=@{{{filepath}}};type=text/plain" -F "apiKey={{{apiKey}}}" -F "projectId={{{projectId}}}" -F "fileType=json" -F "fileUri={{{filepath}}}" {{{localesToApprove}}} -F "callbackUrl={{{callbackUrl}}}" "https://api.smartling.com/v1/file/upload"', curlOptions);
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
 
-
-child = exec(command, function(error, stdout, stderr){
-if(error !== null)
-{
-    console.log('whoops:' + error);
-}
-else
-{
-	console.log(stdout)
-	var contextualizer = new Contextualizer;
-    contextualizer.contextualize(text, avatar, image)
-}
-var newbody = stdout
-
+  console.log(body);
+  var contextualizer = new Contextualizer;
+  contextualizer.contextualize(text, avatar, image);
 });
-};
+
 module.exports = Uploader;
